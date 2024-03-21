eea6878ae452c7a10b8c3a18b80eee5e472c703c
mary <Mariashee001@gmail.com>
2024-03-21 10:49:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// Define the saturdayFun function with default activity
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function with default activity
+  let mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(flair = '*') {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Test the defined functions
+  console.log(saturdayFun()); // Output: This Saturday, I want to roller-skate!
+  console.log(mondayWork()); // Output: This Monday, I will go to the office.
+  const awesome = wrapAdjective('||');
+  console.log(awesome('awesome')); // Output: You are ||awesome||!
+   
